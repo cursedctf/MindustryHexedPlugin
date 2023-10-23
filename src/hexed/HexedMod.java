97cd3c1d7531eea681ea3d474a8414042410a8ff
Ryan Hileman <lunixbochs@gmail.com>
2023-10-23 09:30:34 -0700

    use inline hexToBytes

--- src/hexed/HexedMod.java
+++ src/hexed/HexedMod.java
@@ -257,8 +257,15 @@ public class HexedMod extends Plugin{
         }
         return new String(hexChars, StandardCharsets.UTF_8);
     }
-    private static byte[] hexToBytes(String hex) {
-        return javax.xml.bind.DatatypeConverter.parseHexBinary(hex);
+
+    private static byte[] hexToBytes(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                                 + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
     }
 
     @Override
